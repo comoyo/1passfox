d29d6462eaeaec00d9f867e30c7ddab92358e95d
Sergi Mansilla <sergi.mansilla@gmail.com>
2013-12-04 15:17:41 +0100

    Implement detail view for all fields, and improve concealed field rules

--- www/js/controllers/detail.js
+++ www/js/controllers/detail.js
@@ -3,8 +3,14 @@ define(['app'], function(app) {
     function DetailCtrl($scope, $routeParams, $http) {
       var kc = $scope.keyChain;
       var uuid = $routeParams.uuid;
+      var encryptedFields = [ 'password', 'cvv', 'pin'];
+
+      function isMasked(f) {
+        return encryptedFields.indexOf(f) > -1;
+      }
+
       $http.get('/data/default/' + uuid + '.1password').success(function(entry) {
-        var item = kc.getItem(entry);
+        var item = $scope.item = kc.getItem(entry);
         var decryption_status;
         try {
           decryption_status = kc.decryptItem(item);
@@ -18,17 +24,30 @@ define(['app'], function(app) {
           return;
         }
 
-        $scope.item = item;
-        $scope.item.username = kc.findItemFieldWithDesignation($scope.item, 'username');
-        $scope.item.password = kc.findItemFieldWithDesignation($scope.item, 'password');
+        var decryptedFields = item.decrypted_secure_contents;
+
+        if (decryptedFields.fields) {
+          decryptedFields.fields.forEach(function(f) {
+            f.pwdType = isMasked(f) ? 'password' : 'text';
+            f.concealed = isMasked(f);
+          });
+          $scope.fields = decryptedFields.fields;
+        }
+        else {
+          $scope.fields = Object.keys(decryptedFields).map(function(f) {
+            return {
+              designation: f,
+              value: decryptedFields[f],
+              pwdType: isMasked(f) ? 'password' : 'text',
+              concealed: isMasked(f)
+            };
+          })
+        }
       });
 
-      $scope.pwdType = "password";
-      $scope.revealPass = function() {
-        if ($scope.pwdType === "password")
-          $scope.pwdType = "text";
-        else
-          $scope.pwdType = "password"
+      $scope.revealPass = function(f) {
+        f.pwdType = f.pwdType === 'password' ? 'text' : 'password';
+        $scope.$$phase || $scope.$apply();
       }
     }
   ]);
