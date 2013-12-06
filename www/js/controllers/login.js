4327b91f9ef6b55a9b86536ed1d08b8108bd5d64
Sergi Mansilla <sergi.mansilla@gmail.com>
2013-12-06 13:37:52 +0100

    Simplify login logic

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -2,42 +2,23 @@ define(['app'], function(app) {
   app.controller('LoginCtrl', ['$scope', '$rootScope', '$location',
     function LoginCtrl($scope, $rootScope, $location) {
       $scope.pwd = '';
-      $scope.readyToLogin = false;
 
       function handleError(error) {
         setReady(false);
         $scope.errorMsg = error;
       }
 
-      var authenticated = true;
       function setReady(readyOrNot) {
-        if ($scope.readyToLogin === readyOrNot) return;
-        $scope.readyToLogin = readyOrNot;
-        $scope.$$phase || $scope.$apply();
+        if ($rootScope.readyToLogin === readyOrNot) return;
+        $rootScope.readyToLogin = readyOrNot;
       }
 
-      console.log("ASD")
-
-      $scope.DropboxClient.authenticate({interactive: false}, function(error, client) {
+      $scope.DropboxClient.authenticate(function(error, client) {
         if (error) {
           return handleError(error);
         }
 
-        if (client.isAuthenticated()) {
-          // Cached credentials are available, make Dropbox API calls.
-          alert("AUTHENTICATED ALREADY")
-          setReady(true);
-        } else {
-          // show and set up the "Sign into Dropbox" button
-          alert("TRYING TO AUTHENTICATE")
-          client.authenticate(function(error, client) {
-            alert("AUTHENTICATED!")
-            if (error) {
-              return handleError(error);
-            }
-            setReady(true);
-          });
-        }
+        setReady(true);
       });
 
       $scope.verifyPassword = function() {
