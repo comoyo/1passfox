5715869b2dd5e90c0dd5be597b0081baed503f52
Sergi Mansilla <sergi.mansilla@gmail.com>
2014-01-15 16:54:22 +0100

    add real URLs, not localhost

--- js/lib/dropbox.js
+++ js/lib/dropbox.js
@@ -23,7 +23,7 @@ cloud.dropbox.auth = new Dropbox.Client({
 
 cloud.dropbox.auth.authDriver(new Dropbox.AuthDriver.Popup({
   rememberUser: true,
-  receiverUrl: "http://localhost:8000/dropbox.html"
+  receiverUrl: "http://comoyo.github.io/1passfox/dropbox.html"
 }));
 
 cloud.dropbox.auth.onAuth = new CustomEvent('cloud.dropbox.authed');
